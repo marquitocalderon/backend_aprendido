31ef50e1eb45634ec7c5c9302e41aa914b815424
Marco Calderon <marco.calderon1701@hotmail.com>
2024-01-28 18:12:38 -0500

    conexion a la nube

--- src/conexionBaseDatos/conexion.ts
+++ src/conexionBaseDatos/conexion.ts
@@ -1,13 +1,21 @@
-import { Client } from "pg";
+import { Client , Pool } from "pg";
 
-// Configuración de la conexión a PostgreSQL
-const conectar = new Client({
-  user: 'postgres',
-  host: 'localhost',
-  database: 'prueba',
-  password: 'marquinho1701',
-  port: 5432, // Puerto predeterminado de PostgreSQL
-});
+// // Configuración de la conexión a PostgreSQL
+// const conectar = new Client({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'prueba',
+//   password: 'marquinho1701',
+//   port: 5432, // Puerto predeterminado de PostgreSQL
+// });
+
+
+
+const conectar = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  // ssl:true
+})
+
 
 // Función para conectar y manejar la lógica de conexión
 export async function conectarBaseDeDatos(): Promise<void> {
